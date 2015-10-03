ffcaf2c310dd57f7d77f0c36921c0ef6c51cb009
Stefan Berggren <nsg@nsg.cc>
2015-10-03 22:03:23 +0200

    Moved window logic to a class with a namespace.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,35 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+namespace Konstructs {
+
+    class Window {
+        GLFWwindow* window;
+
+        public:
+
+        Window() {
+            window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+        }
+
+        ~Window() {
+            glfwDestroyWindow(window);
+        }
+
+        int is_ok() {
+            if (window) return 1;
+            return 0;
+        }
+
+        GLFWwindow* get() {
+            return window;
+        }
+    };
+
+}
+
+using namespace Konstructs;
+
 int main(int argc, char **argv) {
 
     if (glfwInit() == GL_FALSE) {
@@ -9,16 +38,19 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-    if (!window) {
+    Window* win = new Window();
+
+    if (!win->is_ok()) {
         glfwTerminate();
         return -1;
     }
 
-    while (!glfwWindowShouldClose(window)) {}
+    while (!glfwWindowShouldClose(win->get())) {
+        // Do stuff ...
+    }
 
-    glfwDestroyWindow(window);
     glfwTerminate();
+    delete win;
     printf("All OK!\n");
     return 0;
 }
