d2c4a3e809eb02aa754dc1d49a3df228e21b733e
Stefan Berggren <nsg@nsg.cc>
2015-10-04 15:56:25 +0200

    Working code.
    - Add tinycthread to src/
    - Rename and build noise
    - Link with ZLIB
    - Load old_main from main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,13 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <tinycthread/tinycthread.h>
 #include "gui.h"
 
 using namespace Konstructs;
 
+int old_main(int argc, char **argv);
+
 int main(int argc, char **argv) {
-
-    if (glfwInit() == GL_FALSE) {
-        printf("Failed to init glfw");
-        return -1;
-    }
-
-    Window* win = new Window();
-
-    if (!win->is_ok()) {
-        glfwTerminate();
-        return -1;
-    }
-
-    while (!glfwWindowShouldClose(win->get())) {
-        // Do stuff ...
-    }
-
-    glfwTerminate();
-    delete win;
-    printf("All OK!\n");
+    old_main(argc, argv);
     return 0;
 }
